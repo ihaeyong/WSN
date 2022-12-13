07994b5a07512648d15a41c0d857f1d07a5be4ad
Haeyong Kang <ihaeyong@gmail.com>
2022-12-13 19:51:49 +0900

    Update wsn_cifar100_100.sh

--- scripts/wsn/wsn_cifar100_100.sh
+++ scripts/wsn/wsn_cifar100_100.sh
@@ -1,10 +1,11 @@
+# Sample Usage: wsn_cifar100_100.sh [GPU_ID] [SEED_NUM] [SPARSITY]
 export PYTHONIOENCODING=utf-8
 export CUDA_VISIBLE_DEVICES="$1"
 
 python3 main_wsn_cifar100_100.py \
     --optim adam \
     --dataset cifar100_100 \
-    --seed 1 \
+    --seed $2 \
     --gpu $1 \
     --lr 1e-3 \
     --lr_min 1e-6 \
@@ -15,5 +16,5 @@ python3 main_wsn_cifar100_100.py \
     --batch_size_test 64 \
     --model alexnet \
     --pc_valid 0.05 \
-    --sparsity 0.5 \
+    --sparsity $3 \
     --encoding huffman
